f1620bcf110aad1c50567656f9c977121f30e0d3
Mayur Agarwal <mayur.m.agarwal@gmail.com>
2025-07-29 19:35:44 +0530

    added wsgi support

--- app.py
+++ app.py
@@ -157,4 +157,5 @@ def reset_quiz():
         participant["score"] = 0
 
 if __name__ == '__main__':
+    print("Starting Real-Time Quiz Application...")
     socketio.run(app, host='0.0.0.0', port=7860, debug=False)
